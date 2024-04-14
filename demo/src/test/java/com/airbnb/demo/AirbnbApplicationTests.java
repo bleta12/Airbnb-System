786bfbef68085c5b0ed294e6a769c908b9254f6e
EdinaBajraktari <150193543+EdinaBajraktari@users.noreply.github.com>
2024-04-14 20:35:05 +0200

    Ndryshime

---
+++ demo/src/test/java/com/airbnb/demo/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
