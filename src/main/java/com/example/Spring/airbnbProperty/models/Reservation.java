cdcb54a2452b32acbcd1b7ba4ad95a952193c1dc
riinesh <rs67349@ubt-uni.net>
2024-04-23 16:04:40 +0200

    Reserve-Table

--- src/main/java/com/example/Spring/airbnbProperty/models/Reservation.java
+++ src/main/java/com/example/Spring/airbnbProperty/models/Reservation.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:src/main/java/com/example/Spring/airbnbProperty/models/Reservation.java
 package com.example.Spring.airbnbProperty.models;
 
 import jakarta.persistence.*;
@@ -22,32 +21,3 @@ public class Reservation {
 }
 
 
-=======
-package airbnbProperty.models;
-
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-
-
-@Getter
-@Setter
-@Entity
-
-@Table(name="Reserve")
-
-public class Reservation {
-    @Id
-
-
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private double  cmimi;
-    private String Lista;
-    private int checkin;
-    private int checkout;
-
-}
->>>>>>> Stashed changes:src/main/java/airbnbProperty/models/Reservation.java
