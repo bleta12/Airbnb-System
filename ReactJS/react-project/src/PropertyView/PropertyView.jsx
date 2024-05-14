0069cbefa4d4860f85a45fdfab22b5e87e40ccda
bleta12 <102874311+bleta12@users.noreply.github.com>
2024-05-14 03:29:51 +0200

    changes in propertyView

--- ReactJS/react-project/src/PropertyView/PropertyView.jsx
+++ ReactJS/react-project/src/PropertyView/PropertyView.jsx
@@ -1,36 +1,112 @@
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import Navbar from "../NavbarFooter/Navbar";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const PropertyView = () => {
-    const navigate = useNavigate();
-    return ( 
-        <>
-        <p className="display-5 text-success ps-5 ms-5 mt-3">Hi from propertyview</p>
-         
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, 
+    slidesToScroll: 1
+  };
 
-         <div className="container-xl my-5">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Reprehenderit eum, veritatis hic expedita repellendus 
-            sapiente? Expedita, id facilis obcaecati non aspernatur repellat,
-             excepturi eum eligendi ut tenetur, fuga adipisci deserunt?</p>
-         </div>
-
-         <div className="container my-5">
+  return (
+    <>
+      <Navbar />
+      <div className="container-xl " >
+        <div className="mt-5 bg-light bg-gradient row ">
+          <div className="col-md-6 mt-2">
+            <img
+              src="https://www.hollywoodreporter.com/wp-content/uploads/2023/04/IMG-Academy-Stadium.jpg?w=1296"
+              className="img-thumbnail img-fluid border rounded"
+              alt="..."
+              style={{ width: "100%", height: "500px" }}
+            />
+          </div>
+          <div className="col-md-6 d-none d-md-block mt-2">
             <div className="row">
-                <div className="col-5 bg-success me-2 ms-2">Div 1</div>
-                <div className="col-2 bg-primary me-2">Div 2</div>
-                <div className="col-2 bg-danger me-2">Div 3</div>
-                <div className="col-2 bg-secondary me-2">Div 4</div>
+              <div className="col">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "250px" }}
+                />
+              </div>
+              <div className="col">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "250px" }}
+                />
+              </div>
             </div>
-         </div>
-         <div>
-      
-      <button  className="custom-button" onClick={() => navigate("/Reservation/reservation")}>
-       <span>Reserve</span> 
-      </button>
-    </div>
-        
-        </>
-     );
-}
- 
+            <div className="row mt-2">
+              <div className="col">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "250px" }}
+                />
+              </div>
+              <div className="col">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "250px" }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 d-block d-md-none">
+            <Slider {...settings}>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail object-fit-md-contain img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail object-fit-md-contain img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail object-fit-md-contain img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <div>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&usqp=CAU"
+                  className="img-thumbnail object-fit-md-contain img-fluid border rounded"
+                  alt="..."
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+            </Slider>
+          </div>
+        </div>
+        <div className="mt-4">
+            <p>TITLE</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default PropertyView;
