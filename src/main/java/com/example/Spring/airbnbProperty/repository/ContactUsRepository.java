e5b2df08300871e234d3f6d9510e7b5963f4bc49
bleta12 <102874311+bleta12@users.noreply.github.com>
2025-01-29 10:28:27 +0100

    Revert "ContactUs"
    This reverts commit c05cd5e2cf439fec3c00c318edf54ea228ded08d.

--- src/main/java/com/example/Spring/airbnbProperty/repository/ContactUsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Spring.airbnbProperty.repository;
-
-import com.example.contactform.models.ContactUs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactUsRepository extends JpaRepository<ContactUs, Long> {
-}
